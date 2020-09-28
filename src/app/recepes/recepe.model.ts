2151a6d9b4ff6ae6bdb2a2539e777cc00a18d1e6
ilyassR <ilyass.rahmoune@gmail.com>
2020-09-28 14:20:40 +0200

    RecepeList component

---
+++ src/app/recepes/recepe.model.ts
@@ -0,0 +1,11 @@
+export class Recepe {
+  public name: string;
+  public description: string;
+  public imagePath: string;
+
+  constructor(name: string, desc: string, imgPath: string) {
+    this.name = name;
+    this.description = desc;
+    this.imagePath = imgPath;
+  }
+}
--- src/app/recepes/recepeList/recepeList.component.ts
+++ src/app/recepes/recepeList/recepeList.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Recepe } from '../recepe.model';
 
 @Component({
   selector: 'app-recepeList',
@@ -6,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrls: ['./recepeList.component.css']
 })
 export class RecepeListComponent {
+  recepes: Recepe[] = [
+    new Recepe('name1', 'description1', 'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg'),
+    new Recepe('name2', 'description2', 'https://www.momontimeout.com/wp-content/uploads/2018/11/chicken-stir-fry-733x1103.jpg')
+  ];
 
 }

49a984c257e4afddaa8e4feeccd0e2e63774b610
ilyassR <ilyass.rahmoune@gmail.com>
2020-09-28 23:59:15 +0200

    image recipe 2

--- src/app/recepes/recepeList/recepeList.component.ts
+++ src/app/recepes/recepeList/recepeList.component.ts
@@ -9,7 +9,7 @@ import { Recepe } from '../recepe.model';
 export class RecepeListComponent {
   recepes: Recepe[] = [
     new Recepe('name1', 'description1', 'https://www.seriouseats.com/2019/07/20190618-grilled-turkish-chicken-wings-vicky-wasik-13-1500x1125.jpg'),
-    new Recepe('name2', 'description2', 'https://www.momontimeout.com/wp-content/uploads/2018/11/chicken-stir-fry-733x1103.jpg')
+    new Recepe('name2', 'description2', 'https://cdn.apartmenttherapy.info/image/upload/f_jpg,q_auto:eco,c_fill,g_auto,w_1500,ar_12:9/k%2FPhoto%2FRecipes%2F2020-02-Kielbasa-and-Cabbage-Skillet%2Fkielbasa2')
   ];
 
 }

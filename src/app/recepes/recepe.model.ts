31e3a93a5f8fef7eb8d1725b876e163cb9f5ee95
ilyassR <ilyass.rahmoune@gmail.com>
2020-09-30 09:43:40 +0200

    add filed isActive

--- src/app/recepes/recepe.model.ts
+++ src/app/recepes/recepe.model.ts
@@ -2,10 +2,12 @@ export class Recepe {
   public name: string;
   public description: string;
   public imagePath: string;
+  public isActive: boolean;
 
-  constructor(name: string, desc: string, imgPath: string) {
+  constructor(name: string, desc: string, imgPath: string, isActive: boolean) {
     this.name = name;
     this.description = desc;
     this.imagePath = imgPath;
+    this.isActive = isActive;
   }
 }
